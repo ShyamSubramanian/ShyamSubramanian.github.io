dacec425c2a1c578e623541bd58f0e7aa27593cb
Shyam Subramanian <shyamsubramanian24@gmail.com>
2018-12-11 08:54:35 -0500

    testing overall page content

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ var scrolllistener = function(event){
     }
 }
 
+var expandcollapse = function(){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].onclick= function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+            content.style.maxHeight = null;
+            } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            } 
+        }
+    }
+}
+
 window.onload = function(event){
-    setTimeout(() => {
-        document.getElementsByTagName('body')[0].onscroll = scrolllistener;
-    }, 500);
+    document.getElementsByTagName('body')[0].onscroll = scrolllistener;
+    expandcollapse()
 }
