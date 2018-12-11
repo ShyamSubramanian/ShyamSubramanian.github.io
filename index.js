b66cffff37a4767af7acf9db846a09bc8baeb7b3
Shyam Subramanian <shyamsubramanian24@gmail.com>
2018-12-11 10:36:44 -0500

    testing overall page content

--- index.js
+++ index.js
@@ -9,24 +9,60 @@ var scrolllistener = function(event){
 var attachEvents = function(){
     document.getElementById('edu').onclick = function(){
         document.getElementsByTagName('body')[0].scrollTop = 102 + document.getElementById('education').offsetTop
+        if(document.getElementById('education').children[0].classList.contains('active')==false){
+            document.getElementById('education').children[0].classList.toggle('active')
+            setheightforexpandcollapse(document.getElementById('education').children[0].nextElementSibling)
+        }
     }
     document.getElementById('cou').onclick = function(){
         document.getElementsByTagName('body')[0].scrollTop = 102 + document.getElementById('courses').offsetTop
+        if(document.getElementById('courses').children[0].classList.contains('active')==false){
+            document.getElementById('courses').children[0].classList.toggle('active')
+            setheightforexpandcollapse(document.getElementById('courses').children[0].nextElementSibling)
+        }
     }
     document.getElementById('ski').onclick = function(){
         document.getElementsByTagName('body')[0].scrollTop = 102 + document.getElementById('skills').offsetTop
+        if(document.getElementById('skills').children[0].classList.contains('active')==false){
+            document.getElementById('skills').children[0].classList.toggle('active')
+            setheightforexpandcollapse(document.getElementById('skills').children[0].nextElementSibling)
+        }
     }
     document.getElementById('exp').onclick = function(){
         document.getElementsByTagName('body')[0].scrollTop = 102 + document.getElementById('workexp').offsetTop
+        if(document.getElementById('workexp').children[0].classList.contains('active')==false){
+            document.getElementById('workexp').children[0].classList.toggle('active')
+            setheightforexpandcollapse(document.getElementById('workexp').children[0].nextElementSibling)
+        }
     }
     document.getElementById('cer').onclick = function(){
         document.getElementsByTagName('body')[0].scrollTop = 102 + document.getElementById('certi').offsetTop
+        if(document.getElementById('certi').children[0].classList.contains('active')==false){
+            document.getElementById('certi').children[0].classList.toggle('active')
+            setheightforexpandcollapse(document.getElementById('certi').children[0].nextElementSibling)
+        }
     }
     document.getElementById('aca').onclick = function(){
         document.getElementsByTagName('body')[0].scrollTop = 102 + document.getElementById('projects').offsetTop
+        if(document.getElementById('projects').children[0].classList.contains('active')==false){
+            document.getElementById('projects').children[0].classList.toggle('active')
+            setheightforexpandcollapse(document.getElementById('projects').children[0].nextElementSibling)
+        }
     }
     document.getElementById('con').onclick = function(){
         document.getElementsByTagName('body')[0].scrollTop = 102 + document.getElementById('contact').offsetTop
+        if(document.getElementById('contact').children[0].classList.contains('active')==false){
+            document.getElementById('contact').children[0].classList.toggle('active')
+            setheightforexpandcollapse(document.getElementById('contact').children[0].nextElementSibling)
+        }
+    }
+}
+
+var setheightforexpandcollapse = function(content){
+    if (content.style.maxHeight){
+    content.style.maxHeight = null;
+    } else {
+    content.style.maxHeight = content.scrollHeight + "px";
     }
 }
 
